dfeee2bddfe7a4fc7e54fc76001db75db7f45af3
itsraulfraire <rfrairegaytan@gmail.com>
2025-09-02 18:13:21 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -67,6 +67,30 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
 app.controller("appCtrl", function ($scope, $http) {
 })
 app.controller("productosCtrl", function ($scope, $http) {
+    $(document).on("submit", "#frmProducto", function (event) {
+        event.preventDefault()
+
+        $.post("/producto", {
+            id: "",
+            nombre: $("#txtNombre").val(),
+            precio: $("#txtPrecio").val(),
+            existencias: $("#txtExistencias").val(),
+        }, function (respuesta) {
+            //
+        })
+    })
+
+    $(document).on("click", ".btn-ingredientes", function (event) {
+        const id = $(this).data("id")
+
+        $.get(`/productos/ingredientes/${id}`, function (html) {
+            modal(html, "Ingredientes", [
+                {html: "Aceptar", class: "btn btn-secondary", fun: function (event) {
+                    closeModal()
+                }}
+            ])
+        })
+    })
 })
 
 const DateTime = luxon.DateTime
@@ -86,3 +110,4 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
+
